d32ce7656bb5625f395733f87c59f7b9a560c91d
popovycha <artem220397@gmail.com>
2023-09-26 20:35:05 -0400

    2nd commit

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
+import { Response } from 'src/app/interface/response.interface';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  response: Response;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers(15).subscribe(
+      (results: Response) => {
+        console.log(results);
+        this.response = results;
+      }
+    );
+  }
 
 }
